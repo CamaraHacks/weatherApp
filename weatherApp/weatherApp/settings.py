d668f202333cdaea15614bfa2b343bb836fc233e
gustavo <gustavo-camara@outlook.com>
2024-08-05 23:49:35 -0300

    ~runserver

--- weatherApp/weatherApp/settings.py
+++ weatherApp/weatherApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b518059$d@ry#w)fmg#v9ue&v-3*ogfp63#(%*t7==l-i$32m1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
