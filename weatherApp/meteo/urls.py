0e34e8a9fdd1fc842a8ee22dcafa2713f8f5d696
gustavo <gustavo-camara@outlook.com>
2024-08-06 00:09:33 -0300

    index added

--- weatherApp/meteo/urls.py
+++ weatherApp/meteo/urls.py
@@ -5,7 +5,7 @@ from meteo import views
 urlpatterns = (
     path("meteo/", views.temp_here, name="temp_here"),
     path("meteo/discover", views.temp_somewhere, name="temp_somewhere"),
-    path("https://meutempo.up.railway.app/", views.temp_somewhere, name="temp_somewhere"),
-    path("https://meutempo.up.railway.app/", views.temp_here, name="temp_here"),
+    path("meutempo.up.railway.app/", views.temp_somewhere, name="temp_somewhere"),
+    path("meutempo.up.railway.app/", views.temp_here, name="temp_here"),
 )
 
