4f99572c421e2aafb829f28b627dd940eb009da4
gustavo <gustavo-camara@outlook.com>
2024-08-05 23:37:38 -0300

    ~runserver

--- weatherApp/weatherApp/settings.py
+++ weatherApp/weatherApp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b518059$d@ry#w)fmg#v9ue&v-3*ogfp63#(%*t7==l-i$32m1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
